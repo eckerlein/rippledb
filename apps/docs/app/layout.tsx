69601c6e73cc74a35f026d5665c35ff38e816f4e
Jan Eckerlein <eckerlein.jan@gmail.com>
2026-01-27 01:48:55 +0100

    feat: update favicon with larger icons and theme-aware metadata

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -2,12 +2,30 @@ import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
 import SearchDialog from '@/components/search';
+import { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
   adjustFontFallback: true,
 });
+
+export const metadata: Metadata = {
+  title: 'RippleDB',
+  description: 'RippleDB is a database for the modern web.',
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/icon-variations/icon-light.svg',
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/icon-variations/icon-dark.svg',
+      },
+    ],
+  },
+};
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
